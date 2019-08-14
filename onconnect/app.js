91253cc7cd144eaf430887c4a6efbd589218bdc7
Vagrant Default User <vagrant@stretch.localdomain>
2019-08-14 18:49:41 +0000

    Add ChannelManager class to onconnect

--- onconnect/app.js
+++ onconnect/app.js
@@ -5,6 +5,49 @@ var AWS = require("aws-sdk");
 AWS.config.update({ region: process.env.AWS_REGION });
 var DDB = new AWS.DynamoDB({ apiVersion: "2012-10-08" });
 
+class ChannelManager = {
+  constructor() {
+    this.channels = this.getChannels();
+  }
+
+  getChannels() {
+    var getParams = {
+      TableName: process.env.CHANNEL_TABLE_NAME
+    };
+    return DDB.scan(getParams);
+  }
+
+  getOpenChannels() {
+    var getParams = {
+      TableName: process.env.CHANNEL_TABLE_NAME,
+      FilterExpression: "open = true",
+    };
+    return DDB.scan(getParams);
+  }
+
+  lockChannel(channelNum, callback) {
+    var putParams = {
+      TableName: process.env.CHANNEL_TABLE_NAME,
+      Key: {
+        N: `${channelNum}`
+      },
+      ExpressionAttributeValues: {
+        ":channelNum": {
+          N: channelNum
+        }
+      },
+      FilterExpression: "channelNum = :channelNum",
+      UpdateExpression: "SET open = false, "
+    };
+    var err = DDB.updateItem(putParams, function (err) {
+      callback(null, {
+        statusCode: err ? 500 : 200,
+        body: err ? "Failed to lock channel: " + JSON.stringify(err) : "Channel locked."
+      }
+    });
+  }
+}
+
 exports.handler = function (event, context, callback) {
   var putParams = {
     TableName: process.env.TABLE_NAME,
